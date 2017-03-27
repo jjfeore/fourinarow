dd964513f8607fa8539b1c295e72d6a48c436c94
James Feore <jjfeore@gmail.com>
2017-03-27 15:36:32 -0700

    Added some themes to the JS, but still no functionality

--- theme.js
+++ theme.js
@@ -5,5 +5,14 @@ var playerOne = document.getElementById('player-one');
 var playerTwo = document.getElementById('player-two');
 var pageBox = document.getElementById('theme-select');
 var theButton = document.getElementById('theme-button');
+var selectBox = document.getElementById('select-box');
+
+var defaultTheme = ['#fdfe02', '#011efe', '#000000', '#FF0000'];
+var darkTheme = ['#000000', '#666666', '#FFFFFF', '#333333'];
+var lightTheme = ['#FFFFFF', '#8f8f8f', '#666666', '#d6d6d6'];
 
 theButton.addEventListener('click', changeTheme);
+
+function changeTheme() {
+  // change the theme
+}
