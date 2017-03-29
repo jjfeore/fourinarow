ba7c3da72c74117d633e9e75dd1b9f15b84e85d1
Bret Ldenburg <bret.ladenburg@gmail.com>
2017-03-29 13:00:21 -0700

    made changes to the theme.js and theme.html files to add functionality to the custom theme feature

--- theme.js
+++ theme.js
@@ -6,17 +6,19 @@ var playerTwo = document.getElementById('player-two');
 var pageBox = document.getElementById('theme-select');
 var theButton = document.getElementById('theme-button');
 var selectBox = document.getElementById('select-box');
+var customThemeBox = document.getElementById('custom-theme');
 
 var classicTheme = ['#011efe', '#fdfe02', '#000000', '#FF0000'];
 var darkTheme = ['#000000', '#666666', '#FFFFFF', '#666666'];
 var lightTheme = ['#FFFFFF', '#8f8f8f', '#000000', '#666666'];
 var dolphinTheme = ['#008D97', '#F5811F', '#FFFFFF', '#183048'];
 var seahawkTheme = ['#002244', '#69BE28', '#A5ACAF', '#FFFFFF'];
+var customTheme = [];
+var selectedOption = document.getElementById('select-box').value;
 
 theButton.addEventListener('click', changeTheme);
 
 function changeTheme() {
-  var selectedOption = document.getElementById('select-box').value;
   if (selectedOption === 'dark') {
     localStorage.customTheme = JSON.stringify(darkTheme);
     setTheme();
@@ -29,6 +31,9 @@ function changeTheme() {
   } else if (selectedOption === 'seahawk') {
     localStorage.customTheme = JSON.stringify(seahawkTheme);
     setTheme();
+  } else if (selectedOption === 'custom') {
+    localStorage.customTheme = JSON.stringify(customTheme);
+    setTheme();
   } else {
     localStorage.customTheme = JSON.stringify(classicTheme);
     setTheme();
@@ -36,3 +41,22 @@ function changeTheme() {
 }
 
 theButton.addEventListener('click', changeTheme);
+
+customThemeBox.style.display = 'none';
+if (selectedOption === 'custom') {
+  selectedOption.addEventListener('click', displayBox);
+}
+
+function displayBox() {
+  if (customThemeBox.style.display === 'none') {
+    if (resetBox) { resetBox.style.display = 'none'; }
+    customThemeBox.setAttribute('style', 'display: block');
+  }
+  else {
+    customThemeBox.style.display = 'none';
+  }
+}
+
+function hideBox() {
+  customThemeBox.setAttribute('style', 'display: none');
+}
