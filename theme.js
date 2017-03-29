0a500dc165796e05a1bd3a1f6acedab4abe175f1
James Feore <jjfeore@gmail.com>
2017-03-29 15:04:59 -0700

    More styling and started the actuall code for the theme creator

--- theme.js
+++ theme.js
@@ -4,36 +4,25 @@ var theBody = document.getElementsByTagName('body')[0];
 var playerOne = document.getElementById('player-one');
 var playerTwo = document.getElementById('player-two');
 var pageBox = document.getElementById('theme-select');
+var themeBox = document.getElementById('custom-theme');
 var theButton = document.getElementsByTagName('button')[0];
 var custButton = document.getElementsByTagName('button')[1];
 var selectBox = document.getElementById('select-box');
 
-var classicTheme = ['#011efe', '#fdfe02', '#000000', '#FF0000'];
-var darkTheme = ['#000000', '#666666', '#FFFFFF', '#222222'];
-var lightTheme = ['#FFFFFF', '#8f8f8f', '#000000', '#666666'];
-var dolphinTheme = ['#008D97', '#F5811F', '#FFFFFF', '#183048'];
-var seahawkTheme = ['#002244', '#69BE28', '#A5ACAF', '#FFFFFF'];
+var themeArray = [['#011efe', '#fdfe02', '#000000', '#FF0000'], ['#000000', '#666666', '#FFFFFF', '#222222'], ['#FFFFFF', '#8f8f8f', '#000000', '#666666'], ['#008D97', '#F5811F', '#FFFFFF', '#183048'], ['#002244', '#69BE28', '#A5ACAF', '#FFFFFF']];
 
+themeBox.style.display = 'none';
 theButton.addEventListener('click', changeTheme);
+custButton.addEventListener('click', createTheme);
 
 function changeTheme() {
   var selectedOption = document.getElementById('select-box').value;
-  if (selectedOption === 'dark') {
-    localStorage.customTheme = JSON.stringify(darkTheme);
-    setTheme();
-  } else if (selectedOption === 'light') {
-    localStorage.customTheme = JSON.stringify(lightTheme);
-    setTheme();
-  } else if (selectedOption === 'dolphin') {
-    localStorage.customTheme = JSON.stringify(dolphinTheme);
-    setTheme();
-  } else if (selectedOption === 'seahawk') {
-    localStorage.customTheme = JSON.stringify(seahawkTheme);
-    setTheme();
-  } else {
-    localStorage.customTheme = JSON.stringify(classicTheme);
+  if (selectedOption < document.getElementsByTagName('option').length - 1) {
+    localStorage.customTheme = JSON.stringify(themeArray[selectedOption]);
     setTheme();
   }
+  else {
+    pageBox.style.display = 'none';
+    themeBox.style.display = 'block';
+  }
 }
-
-theButton.addEventListener('click', changeTheme);
